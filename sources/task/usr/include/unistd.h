b7b16dfdbe51e3fb2aeeffc92c09ba4235b31d06
Tomasz Domagała <tomasz.domagala.contact@gmail.com>
2021-05-01 09:28:04 +0200

    Add setbid declaration to unistd.h

--- sources/task/usr/include/unistd.h
+++ sources/task/usr/include/unistd.h
@@ -144,6 +144,7 @@ __aconst char *ttyname(int);
 int	 unlink(const char *);
 ssize_t	 write(int, const void *, size_t);
 
+int setbid(int);
 
 /*
  * IEEE Std 1003.2-92, adopted in X/Open Portability Guide Issue 4 and later
